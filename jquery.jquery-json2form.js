a22369a8903eb9e28fdbcc43dcac702bacaaf986
saurshaz <saurshaz@gmail.com>
2013-07-12 16:51:47 +0530

    Update jquery.jquery-json2form.js
    added logic to handle verbose as well as concise JSON formats

--- jquery.jquery-json2form.js
+++ jquery.jquery-json2form.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery JSON-2-Form  Plugin v1.0.0
- * https://github.com/saurshaz/jquery.jquery-json2form.js
+ * https://github.com/saurshaz/jquery-json2form
  *
  * Copyright 2013 Saurabh Sharma
  * Released under the MIT license
@@ -15,45 +15,75 @@
     }
 }(function ($) {
 
-    var config = $.populateForm = function (frm, data, options) {
+    var fillForm = function (frm, name, val) {
+        // handle input fields
+        var $el = frm.find($('input[name="' + name + '"]')),
+            type = $el.attr('type');
 
+        switch (type) {
+            case 'checkbox':
+                $el.attr('checked', 'checked');
+                break;
+            case 'radio':
+                $el.filter('[value="' + val + '"]').attr('checked', 'checked');
+                break;
+            default:
+                $el.val(val);
+                break;
+        }
+
+        // handle select fields
+        var $elSelect = frm.find($('select[name="' + name + '"]')),
+            typeOfSelect = $elSelect.attr('multiple');
+
+        if ((typeOfSelect) && (typeOfSelect === 'multiple')) {
+            // handle multi select scenario
+            if (val != undefined && val instanceof Array) {
+                //alert(val+' is an Array');   
+                $elSelect.val(val);
+            } else {
+                $elSelect.find($("option[value='" + val + "']")).prop('selected', true);
+            }
+        } else {
+            // handle single select scenario
+            $elSelect.val(val);
+        }
+    };
+
+    var handleVerboseFormJson = function (frm, data) {
+        // reset form values from json object
+        $.each(data, function (id, node) {
+            var name = node.name;
+            var val = node.value;
+
+            // handle input fields
+            fillForm(frm, name, val);
+        });
+    };
+
+    var handleConciseFormJson = function (frm, data) {
+        // reset form values from json object
+        $.each(data, function (idx, node) {
+            alert(node.name + '=' + node.value);
+            fillForm(frm, node.name, node.value);
+        });
+    };
+
+    var config = $.populateForm = function (frm, data, options) {
         // populate specified form from JSON
         if (data !== undefined && frm !== undefined) {
             options = $.extend({}, config.defaults, options);
 
-            // reset form values from json object
-            $.each(data, function (id, node) {
-                var name = node.name;
-                var val = node.value;
-
-                // handle input fields
-                var $el = frm.find($('input[name="' + name + '"]')),
-                    type = $el.attr('type');
-
-                switch (type) {
-                    case 'checkbox':
-                        $el.attr('checked', 'checked');
-                        break;
-                    case 'radio':
-                        $el.filter('[value="' + val + '"]').attr('checked', 'checked');
-                        break;
-                    default:
-                        $el.val(val);
-                        break;
-                }
-
-                // handle select fields
-                var $elSelect = frm.find($('select[name="' + name + '"]')),
-                    typeOfSelect = $elSelect.attr('multiple');
-
-                if ((typeOfSelect) && (typeOfSelect === 'multiple')) {
-                    // handle multi select scenario
-                    $elSelect.find($("option[value='" + val + "']")).prop('selected', true);
-                } else {
-                    // handle single select scenario
-                    $elSelect.val(val);
-                }
-            });
+            // default format is considered to be concise
+            if (options.format && options.format === 'verbose') {
+                // if the format is [{name:<name_val1>,value:,<val_value1>},{name:<name_val2>,value:,<val_value2>}]
+                handleVerboseFormJson(frm, data);
+            } else if (options.format && options.format === 'provided') {
+                // support any other JSON format TODO :: if needed -- consider the option -- 'format'
+            } else {
+                // if the format is [{<name_val1>:<val_value1>,<name_val2>:<val_value2>}] :: DEFAULT choice
+                handleConciseFormJson(frm, data);
+            }
         }
     };
 }));
